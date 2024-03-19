fc409f68f8549d8cdf0be9421f18f43c142ad838
Ojochogwu Dickson <07afrik@gmail.com>
2024-03-19 20:13:42 +0100

    wip

---
+++ app/agency/page.tsx
@@ -0,0 +1,14 @@
+'use client'
+
+
+
+export default function Agency(){
+    return (
+        <div className=" h-[120vh] w-screen py-10 flex justify-center items-center bg-[#e6e8f4] md:px-[100px] px-[20px] font-raleway">
+            <div className=" bg-slate-100 shadow-xl-200 w-full md:w-1/2 h-[70%] p-8
+            flex justify-center items-start flex-col  my-auto rounded-xl">
+                
+            </div>
+        </div>
+    )
+}
---
+++ app/portfolio/page.tsx
@@ -0,0 +1,14 @@
+'use client'
+
+
+export default function PorfolioPage(){
+    return (
+        <div className=" h-[120vh] w-screen py-10 flex justify-start items-center bg-[#e6e8f4] px-[20px] md:px-[100px] font-raleway">
+            <div className=" bg-slate-100 shadow-xl-200 md:w-1/2 w-full h-full p-8
+            flex justify-center items-start flex-col  my-auto rounded-xl">
+
+               
+            </div>
+        </div>
+    )
+}
